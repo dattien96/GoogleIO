92bba34310c7314112e733e133b9927c9bade5e9
nguyen.ho.tien.dat <FRAMGIA\nguyen.ho.tien.dat@framgia0131-pc.framgia.com>
2018-11-20 13:22:35 +0700

    welcome fragment

--- mobile/src/main/java/dattien96/vn/googleio/ui/onboarding/WelcomeFragment.kt
+++ mobile/src/main/java/dattien96/vn/googleio/ui/onboarding/WelcomeFragment.kt
@@ -20,19 +20,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.core.view.doOnLayout
 import androidx.fragment.app.Fragment
 import dattien96.vn.googleio.R
+import kotlinx.android.synthetic.main.fragment_welcome.*
 
-
-/**
- * First page of onboarding showing a welcome message & branding animation.
- */
 class WelcomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_welcome, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        logo.apply {
+            val interpolator =
+               AnimationUtils.loadInterpolator(context, android.R.interpolator.linear_out_slow_in)
+            alpha = 0.4f
+            scaleX = 0.8f
+            scaleY = 0.8f
+            doOnLayout {
+                translationY = height / 3f
+                animate()
+                    .alpha(1f)
+                    .translationY(0f)
+                    .scaleX(1f)
+                    .scaleY(1f)
+                .interpolator = interpolator
+                playAnimation()
+            }
+        }
     }
 }
